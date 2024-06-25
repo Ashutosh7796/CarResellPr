7fba3f7e7e13872b7d9ac467cf97a3cf2073289e
Ashutosh7796 <ashutoshshedgeas87@gmail.com>
2024-06-25 19:05:49 +0530

    ADDED: Websocket Config and Api

--- JWT-with-Spring-Security/src/main/java/com/spring/jwt/Wallet/Interface/TransactionService.java
+++ JWT-with-Spring-Security/src/main/java/com/spring/jwt/Wallet/Interface/TransactionService.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface TransactionService {
     TransactionDTO credit(Integer accountId, Double amount);
+
     TransactionDTO debit(Integer accountId, Double amount);
+
     TransactionDTO withdraw(Integer accountId, Double amount);
+
     List<TransactionDTO> getByAccountId(Integer accountId);
+
     TransactionDTO getById(Integer transactionId);
 }
